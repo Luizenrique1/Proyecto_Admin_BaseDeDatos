4895974ffa508d57ec1c47b9154a62a10f973517
Luizenrique1 <48364368+Luizenrique1@users.noreply.github.com>
2020-05-04 10:12:34 -0500

    Ventanas inciales
    Se agregaron los codigos bases del sistema ABCC que son ventana de login y ventana incial

---
+++ Proyecto_Bodega/src/Conexion/Conexion.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+/**
+ *
+ * @author Luiz
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import proyecto_bodega.Pantalla;
+public class Conexion {
+    public static Connection con=null;
+    public static Connection getConnection(){
+        //DESKTOP-DMHSAF3 laptop
+    String url = "jdbc:sqlserver://DESKTOP-DDNUBG6:1433;databaseName=Bodega";
+        try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                    
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null,"No se puede establecer conexion.. revisa drivers" + e.getMessage(),
+                    "error de conexion",JOptionPane.ERROR_MESSAGE);
+        }
+    
+        try {
+            con = DriverManager.getConnection(url,"Luiz","12345");
+        } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null,"ERROR" + e.getMessage(),
+                    "error de conexion",JOptionPane.ERROR_MESSAGE);
+        }
+    return con;
+    }
+    
+    public static ResultSet Consulta(String consulta){
+    Connection con = getConnection();
+    Statement declara;
+        try {
+            declara=con.createStatement();
+            ResultSet respuesta = declara.executeQuery(consulta);
+            return respuesta;
+        } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null,"ERROR" + e.getMessage(),
+                    "error de conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        return null;
+    }
+    public static void Ejecutar(String consulta){
+    Connection con = getConnection();
+    Statement declara;
+        try {
+            declara=con.createStatement();
+            declara.executeQuery(consulta);
+            
+        } catch (SQLException e) {
+            if (e.getMessage().equals("La instrucción no devolvió un conjunto de resultados.")) {
+                JOptionPane.showMessageDialog(null,
+                    "Se actualizo el base de datos correctamente");
+               
+            }else{
+            JOptionPane.showMessageDialog(null,"Producto con ese nombre ya existe " ,
+                    "ERROR",JOptionPane.ERROR_MESSAGE);
+                    //System.out.println(e);
+            }
+             
+        }
+        
+    }
+        public static void Eliminar_Producto(String consulta){
+    Connection con = getConnection();
+    Statement declara;
+        try {
+            declara=con.createStatement();
+            declara.executeQuery(consulta);
+            
+        } catch (SQLException e) {
+            if (e.getMessage().equals("La instrucción no devolvió un conjunto de resultados.")) {
+                JOptionPane.showMessageDialog(null,
+                    "Se actualizo el base de datos correctamente");
+               
+            }else{
+            JOptionPane.showMessageDialog(null,"Producto siendo usado en pedidos" ,
+                    "ERROR",JOptionPane.ERROR_MESSAGE);
+                    //System.out.println(e);
+            }
+             
+        }
+        
+    }
+    
+}
---
+++ Proyecto_Bodega/src/proyecto_bodega/grafica.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_bodega;
+
+import java.util.Vector;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import static proyecto_bodega.Tabla.res;
+
+/**
+ *
+ * @author Luiz
+ */
+public class grafica {
+    DefaultPieDataset data = new DefaultPieDataset();
+    Vector v = new Vector();
+    
+        public void obtener_info(){
+        res=Conexion.Conexion.Consulta("SELECT count(tipo), tipo FROM vista_producto GROUP BY tipo;");
+        try {
+            while(res.next()){
+                data.setValue(res.getString(2).toString()+": "+ new Integer(res.getInt(1)), new Integer(res.getInt(1)));
+            }
+                
+        } catch (Exception e) {
+        }
+       JFreeChart chart = ChartFactory.createPieChart("grafica de tipo de productos", data);
+       ChartFrame frame = new ChartFrame("", chart);
+       frame.pack();
+       frame.setIconImage(new javax.swing.ImageIcon(getClass().getResource("base-de-datos.png")).getImage());
+       frame.setSize(400,400);
+       frame.setLocationRelativeTo(null);
+       frame.setVisible(true);
+    }
+    
+
+}
